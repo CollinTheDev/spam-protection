dc99303a1fe716e963b6f8e0661df6a1c6ea581d
CollinTheDev <collin.entringer@gmail.com>
2024-11-28 20:21:30 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Jones'
+description 'This script mutes players for spamming in chat based on message frequency.'
+version '1.0.0'
+
+server_script 'chat_mute.lua'
